d6c6362d2376b5bb31e251105c65dddd6125e590
maahin2005 <mahin.malek2005@gmail.com>
2024-05-04 21:06:42 +0530

    FIX( ): bug

--- Controllers/Products/getSingleProduct.js
+++ Controllers/Products/getSingleProduct.js
@@ -4,7 +4,7 @@ const getSingleProduct = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const product = await ProductModel.findObe({ _id: id });
+    const product = await ProductModel.findOne({ _id: id });
 
     res.status(200).json({ data: product });
   } catch (error) {
