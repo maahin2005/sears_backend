54878fe006f08d5b2a1bf2849709b35e2bbba0f6
maahin2005 <mahin.malek2005@gmail.com>
2024-05-04 20:48:55 +0530

    Single_Product_API

---
+++ Controllers/Products/getSingleProduct.js
@@ -0,0 +1,16 @@
+const ProductModel = require("../../Models/Products");
+
+const getSingleProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await ProductModel.findObe({ _id: id });
+
+    res.status(200).json({ data: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = getSingleProduct;
--- Routers/product.js
+++ Routers/product.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const getProducts = require("../Controllers/Products/getProducts");
 const createProduct = require("../Controllers/Products/createProduct");
+const getSingleProduct = require("../Controllers/Products/getSingleProduct");
 
 const productRoutes = Router();
 
 productRoutes.get("/", getProducts);
+productRoutes.get("/:id", getSingleProduct);
 productRoutes.post("/", createProduct);
 
 module.exports = productRoutes;
