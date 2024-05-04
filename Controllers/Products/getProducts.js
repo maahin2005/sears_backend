0e2939f166a620dbe32615aa5af1752cf0791e90
maahin2005 <mahin.malek2005@gmail.com>
2024-05-04 14:23:20 +0530

    Pagination-&-Filteration

--- Controllers/Products/getProducts.js
+++ Controllers/Products/getProducts.js
@@ -1,10 +1,28 @@
 const ProductModel = require("../../Models/Products");
 
 const getProducts = async (req, res) => {
-  try {
-    const products = await ProductModel.find(req.query);
+  const limit = req.query.limit;
+  const page = req.query.page || 1;
+  const skipping = limit * (page - 1);
+  const query = req.query;
+  let API_URL = ProductModel.find(query);
 
-    res.json({ products });
+  if (req.query.page || req.query.limit) {
+    API_URL = ProductModel.find();
+  }
+  try {
+    const allProducts = await ProductModel.find();
+    const totalProducts = allProducts.length;
+    const products = await API_URL.skip(skipping).limit(limit);
+    const totalPages = Math.ceil(totalProducts / limit) || 1;
+
+    res.json({
+      totalProducts,
+      currentPage: +page,
+      totalPages,
+      productsPerPage: +limit || +products.length,
+      data: products,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
