be72ab83247c3497fb7a9e9f5137c6966ef83175
maahin2005 <mahin.malek2005@gmail.com>
2024-05-06 15:44:35 +0530

    Dynamic-Adv( ):SEARCH

--- Controllers/Products/getProducts.js
+++ Controllers/Products/getProducts.js
@@ -30,35 +30,57 @@ const ProductModel = require("../../Models/Products");
 
 // module.exports = getProducts;
 
+// ------------------------------------------------------------------
+
 const getProducts = async (req, res) => {
-  const limit = req.query.limit;
-  const page = req.query.page || 1;
+  const limit = parseInt(req.query.limit);
+  const page = parseInt(req.query.page) || 1;
   const skipping = limit * (page - 1);
-  const query = req.query;
-  let API_URL = ProductModel.find(query);
+  const searchTerm = req.query.search;
+
+  const singleQuery = req.query;
+
+  let query = {};
 
   // Check if search query is present in request
-  if (req.query.search) {
-    let query = {};
-    query.$text = { $search: req.query.search }; // Using $text operator for text search
-    API_URL = ProductModel.find(query);
+  if (searchTerm) {
+    query.$or = [
+      { tags: { $regex: searchTerm, $options: "i" } }, // Match tags containing the search term (case-insensitive)
+      { category: { $regex: searchTerm, $options: "i" } }, // Match category containing the search term (case-insensitive)
+      { title: { $regex: searchTerm, $options: "i" } }, // Match title containing the search term (case-insensitive)
+      { description: { $regex: searchTerm, $options: "i" } }, // Match description containing the search term (case-insensitive)
+    ];
   }
 
+  if (req.query.category) {
+    query.category = req.query.category;
+  }
+
+  // Filter by price range
+  if (req.query.minPrice && req.query.maxPrice) {
+    query.price = { $gte: req.query.minPrice, $lte: req.query.maxPrice };
+  }
+
+  let API_URL = ProductModel.find(query);
+
   if (req.query.page || req.query.limit) {
-    API_URL = ProductModel.find();
+    API_URL.skip(skipping).limit(limit);
+  }
+  if (Object.keys(singleQuery).length > 0 && Object.keys(query).length === 0) {
+    API_URL = ProductModel.find(singleQuery);
   }
 
   try {
-    const allProducts = await ProductModel.find();
-    const products = await API_URL.skip(skipping).limit(limit);
+    const allProducts = await ProductModel.find(query);
+    const products = await API_URL;
     const totalProducts = allProducts.length;
     const totalPages = Math.ceil(totalProducts / limit) || 1;
 
     res.json({
       totalProducts,
-      currentPage: +page,
+      currentPage: page,
       totalPages,
-      productsPerPage: +limit || +products.length,
+      productsPerPage: products.length,
       data: products,
     });
   } catch (error) {
